a2bec2895af3330ee230e22d19d9c09fb5999167
Meet P. <meetvk57@gmail.com>
2025-05-31 16:12:57 -0700

    unversioned files

---
+++ cpp/example/src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "mugengine.hpp"
+
+void logError(std::string error) {
+    std::cerr << "[MugEngine]\t" << error << std::endl;
+}
+
+void processInput(GLFWwindow* window);
+
+int main(int argv, char** argc) {
+    MugEngine engine = MugEngine();
+    engine.setErrorCallback(logError);
+    engine.init();
+    engine.createWindow(800, 600, "Mugtree", nullptr, nullptr);
+    GLFWwindow* window = engine.getWindow();
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
